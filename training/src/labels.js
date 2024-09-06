e42889f8ce4a4437c222e52f4758f036dc9dd402
Stefano Mavilio <st.mavilio@gmail.com>
2024-09-06 15:48:50 +0200

    feat(training): update labels

--- training/src/labels.js
+++ training/src/labels.js
@@ -1,5 +1,16 @@
-const eyesOpen = [1, 0, 0];
-const eyesClosed = [0, 1, 0];
-const yawning = [0, 0, 1];
+const eyesOpen = {
+    labels: 'eyes-open',
+    encoding: [1, 0, 0]
+};
+
+const eyesClosed = {
+    labels: 'eyes-closed',
+    encoding: [0, 1, 0]
+};
+
+const yawning = {
+    labels: 'yawning',
+    encoding: [0, 0, 1]
+};
 
 module.exports = {eyesOpen, eyesClosed, yawning};
