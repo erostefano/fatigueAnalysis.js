864b9b992d59d2274240e71f0d9130199bdd2441
Stefano Mavilio <st.mavilio@gmail.com>
2024-09-06 17:30:20 +0200

    feat(training): fix data

---
+++ training/src/data.js
@@ -0,0 +1,20 @@
+const { loadImagesFromFolder, splitIntoTrainingData, getDataSet } = require("./util");
+const logger = require("./logger");
+const tf = require("@tensorflow/tfjs-node");
+const { eyesOpen, eyesClosed, yawning } = require("./labels");
+
+const eyesOpenSet = getDataSet(eyesOpen);
+const eyesClosedSet = getDataSet(eyesClosed);
+const yawningSet = getDataSet(yawning);
+
+const eyesOpenTraining = splitIntoTrainingData(eyesOpenSet, eyesOpen);
+const eyesClosedTraining = splitIntoTrainingData(eyesClosedSet, eyesClosed);
+const yawningTraining = splitIntoTrainingData(yawningSet, yawning);
+
+const xTrain = tf.stack(eyesOpenTraining.images.concat(eyesClosedTraining.images));
+logger.info('xTrain shape', xTrain.shape);
+
+const yTrain = tf.tensor2d(eyesOpenTraining.labels.concat(eyesClosedTraining.labels), [xTrain.shape[0], 3]);
+logger.info('yTrain shape', yTrain.shape);
+
+module.exports = { train: { x: xTrain, y: yTrain } };
