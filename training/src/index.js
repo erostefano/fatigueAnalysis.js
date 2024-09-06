6c0187a90a106319b5579f64a32eda9369491962
Stefano Mavilio <st.mavilio@gmail.com>
2024-09-06 20:13:16 +0200

    feat(training): use learning rate for hyper param tuning

--- training/src/index.js
+++ training/src/index.js
@@ -94,7 +94,7 @@ async function hyperParamTuning() {
     for (const activation of ['relu', 'elu', 'tanh', 'sigmoid',]) {
         for (const dropoutRate of [0.2, 0.5, 0.8]) {
             for (const learningRate of [0.01, 0.001, 0.0001, 0.00001, 0.000001]) {
-                logger.info(`Starting with Activation: ${activation}, Dropout: ${dropoutRate}`);
+                logger.info(`Starting with Activation: ${activation}, Dropout: ${dropoutRate}, Learning Rate: ${learningRate}`);
 
                 const cnn = createCnn(activation, dropoutRate, learningRate);
                 const history = await cnn.fit(
